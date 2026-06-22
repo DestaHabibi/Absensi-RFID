c7fd3486908340ffed9441b1262b68219f79b0b1
Intantri27 <trihartatiintan@gmail.com>
2026-06-22 11:57:18 +0100

    ambil LogAbsensi, SerialDataHandler, DigitalClockService, LogAbsensiService, SerialService dari branch indah

---
+++ src/main/java/com/mycompany/absensi_rfid/serial/SerialDataHandler.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.absensi_rfid.serial;
+
+/**
+ *
+ * @author lenovo
+ * @param <T>
+ */
+public interface SerialDataHandler<T> {
+    void onDataReceived(T data); 
+}
